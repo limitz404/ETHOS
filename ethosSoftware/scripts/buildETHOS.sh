4931ae40667f4f6f16b19b556bff42e871337050
Patrick Klein <klein.pat@icloud.com>
2015-03-30 12:23:44 -0600

    updating build script / output structure

--- ethosSoftware/scripts/buildETHOS.sh
+++ ethosSoftware/scripts/buildETHOS.sh
@@ -1,5 +1,5 @@
 ##########################################################################################
-# AUTHORS:      TAYLOR DEAN, PATRICK KLEIN                                               #
+# AUTHORS:      PATRICK KLEIN, TAYLOR DEAN                                               #
 # FILE:         buildEthos.sh                                                            #
 # CREATED:      03-29-2015                                                               #
 # ORGANIZATION: CU-BOULDER, AES SENIOR DESIGN TEAM ETHOS                                 #
@@ -8,24 +8,76 @@
 #               INDEPENDENT OF CURRENT DIRECTORY                                         #
 ##########################################################################################
 
+# options:    clean
+#             optimize
+#             debugging
+#             profiling
 
-#! /bin/bash
+#!/bin/bash
 
-# clean
-rm /root/ethosSoftware/bin/*
-rm /root/ethosSoftware/obj/*
+INC=/usr/include/
+LIB=/usr/lib/
 
-# compile C++ source files and place into ~/ethosSoftware/obj/
-g++ -c -I /root/ethosSoftware/src/common/ -o /root/ethosSoftware/obj/edgeDetection.o /root/ethosSoftware/src/algorithm/edgeDetection.cpp -std=c++0x -g
-g++ -c -I /root/ethosSoftware/src/common/ -o /root/ethosSoftware/obj/attitudeDetermination.o /root/ethosSoftware/src/algorithm/attitudeDetermination.cpp -std=c++0x -g
-g++ -c -I /root/ethosSoftware/src/common/ -o /root/ethosSoftware/obj/common.o /root/ethosSoftware/src/common/common.cpp -g
-g++ -c -I /root/ethosSoftware/src/bitBangTesting/ -I /root/ethosSoftware/src/common/ -I /root/ethosSoftware/src/algorithm/ -o /root/ethosSoftware/obj/main.o /root/ethosSoftware/src/common/main.cpp -std=c++0x -g
+SRC=/root/ethosSoftware/src
+OBJ=/root/ethosSoftware/obj
+OUT=/root/ethosSoftware/output
+BIN=/root/ethosSoftware/bin
 
-# compile C source file and place into ~/ethosSoftware/obj/
-gcc -I ../src/common/ -I /usr/include/ -L /usr/lib/ -c -o /root/ethosSoftware/obj/bitBangController.o /root/ethosSoftware/src/bitBangTesting/bitBangController.c -lpthread -lprussdrv -g
+COMPILE_OPTIONS=""
+LINK_OPTIONS=""
 
-# assemble PRU code and place into ~ethosSoftware/obj/
-pasm -V3 -b /root/ethosSoftware/src/bitBangTesting/bitBang.p /root/ethosSoftware/obj/bitBang > /root/ethosSoftware/output/pasmLog.txt
+# evaluate input arguments
+for arg in $@
+do
 
-# link object files and place into ~ethosSoftware/bin/
-g++ -I /usr/include/ -L /usr/lib/ /root/ethosSoftware/obj/common.o /root/ethosSoftware/obj/edgeDetection.o /root/ethosSoftware/obj/attitudeDetermination.o /root/ethosSoftware/obj/main.o /root/ethosSoftware/obj/bitBangController.o -o /root/ethosSoftware/bin/ethos -lpthread -lprussdrv -g
+    case "$arg" in
+
+    "clean" )    rm $BIN/*
+                 rm $OUT/*.*
+                 rm $OBJ/*
+                 ;;
+
+    "optimize1" )   COMPILE_OPTIONS="$COMPILE_OPTIONS -O1"
+                    LINK_OPTIONS="$LINK_OPTIONS -O1"
+                    ;;
+
+    "optimize2" )   COMPILE_OPTIONS="$COMPILE_OPTIONS -O2"
+                    LINK_OPTIONS="$LINK_OPTIONS -O2"
+                    ;;
+
+    "optimize3" )   COMPILE_OPTIONS="$COMPILE_OPTIONS -O3"
+                    LINK_OPTIONS="$LINK_OPTIONS -O3"
+                    ;;
+
+    "debug" )    COMPILE_OPTIONS="$COMPILE_OPTIONS -g"
+                 LINK_OPTIONS="$LINK_OPTIONS -g"
+                 ;;
+
+    "profile" )    COMPILE_OPTIONS="$COMPILE_OPTIONS -pg"
+                   LINK_OPTIONS="$LINK_OPTIONS -pg"
+                   ;;
+
+    * )    echo "Unrecognized command: $arg"
+                   ;;
+
+    esac
+
+done
+
+# compile C++ source files and place into OBJ
+g++ -c -I $SRC/common/ -o $OBJ/edgeDetection.o $SRC/algorithm/edgeDetection.cpp -std=c++0x $COMPILE_OPTIONS
+g++ -c -I $SRC/common/ -o $OBJ/attitudeDetermination.o $SRC/algorithm/attitudeDetermination.cpp -std=c++0x $COMPILE_OPTIONS
+g++ -c -I $SRC/common/ -o $OBJ/common.o $SRC/common/common.cpp $COMPILE_OPTIONS
+g++ -c -I $SRC/bitBangTesting/ -I $SRC/common/ -I $SRC/algorithm/ -o $OBJ/main.o $SRC/common/main.cpp -std=c++0x $COMPILE_OPTIONS
+
+# compile C source file and place into OBJ
+gcc -I $SRC/common/ -I $INC -L $LIB -c -o $OBJ/bitBangController.o $SRC/bitBangTesting/bitBangController.c -lpthread -lprussdrv $COMPILE_OPTIONS
+
+# assemble PRU code and place into OBJ
+pasm -V3 -b SRC/bitBangTesting/bitBang.p OBJ/bitBang > $OUT/pasmLog.txt
+
+# link object files and place into BIN
+g++ -I $INC -L $LIB $OBJ/common.o $OBJ/edgeDetection.o $OBJ/attitudeDetermination.o $OBJ/main.o $OBJ/bitBangController.o -o $BIN/ethos -lpthread -lprussdrv $LINK_OPTIONS
+
+
+
