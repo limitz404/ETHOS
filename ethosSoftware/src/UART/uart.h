cd648e58b741eec37c7b62622b18334030c1fbb6
maci4699 <matthew.l.cirbo@colorado.edu>
2015-04-16 15:07:35 -0600

    Added UART output and read code
    Added the UART code that will read commands and output the necessary
    data.

---
+++ ethosSoftware/src/UART/uart.h
@@ -0,0 +1,28 @@
+/* Header file that holds any constants and function declerations */
+
+#define UART_H
+#ifdef UART_H
+
+/* Global Constants */
+int fd; // Used for keeping track of the open port
+
+/* Function Delarations */
+
+/************************************************* 
+* Function that starts the uart and opens the port
+* It clears all old port settings and resets the 
+* port using the new settings.
+*************************************************/
+void initUart();
+
+/************************************************* 
+* Function that reads the port line for any data.
+* Ff none is found then it will return a 0.
+*************************************************/
+int readUart();
+
+/************************************************* 
+* Function that writes data to the port that is
+* given from the calling function.
+*************************************************/
+void writeUart(char dataArray[]);
